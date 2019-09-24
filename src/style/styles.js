d05ffbf7f9f0945346d6f930ee8ee74f0fcf2c5d
Petr Novotný <krkabol@gmail.com>
2019-09-24 17:30:51 +0200

    wfs squares

--- src/style/styles.js
+++ src/style/styles.js
@@ -10,6 +10,7 @@ export const styleFunction = {
         return commonStyles[name](radius);
     },
     getSquareText: function (feature, resolution) {
+        console.log(feature);
         let text = feature.get('id').toString();
         if (resolution > 300) {
             text = '';
